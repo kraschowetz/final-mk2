43584475ce3e9c40e88f98d9b50f30a2f9176774
Kraschowetz <joao.kraschowetz@gmail.com>
2023-11-27 00:05:09 -0300

    upd

--- src/javascript/exports.js
+++
@@ -1,7 +0,0 @@
-import styled from 'styled-components';
-
-export const Line = styled.div`
-    display: flex;
-    flex-direction: row;
-    width: 100%;
-`;
--- src/javascript/home.js
+++ src/javascript/home.js
@@ -1,6 +1,6 @@
 import '../css/style.css';
+import styled from 'styled-components';
 import React, { useState, useEffect } from 'react';
-//import { Square } from './exports';
 
 function Home() {
 
@@ -23,6 +23,17 @@ function Home() {
     
   });
 
+  const TextBox = styled.div`
+    background-color: ${props => props.bg};
+    width: ${props => props.width};
+    margin: 20px;
+    padding: 10px;
+    border-radius: 8px;
+    min-height: 30px;
+    height: fit-content;
+    text-align: center;
+  `;
+
   return (
     <div className='container'>
       <header className={vsbl}>
@@ -30,6 +41,43 @@ function Home() {
           SITE PHODA
         </h1>
       </header>
+
+      <img src='img/flower-boy.png' alt='cover' id='img-frame'/>
+
+      <div id='intro-div'>
+        <h1 className='margin-top-h1'>QUEM SOU EU?</h1>
+
+        <TextBox bg="#f1f1f1" width="80%">
+          <p className='text'>
+            Sou Kraschowetz sou estudante do ensino médio e tenho 17 anos. <br/>
+            Gosto de programação, desenho, música, jogos e livros.
+          </p>
+        </TextBox>
+
+        <h1>O QUE FAÇO?</h1>
+
+        <div className='line' align="baseline">
+          <TextBox bg="#f1f1f1" width="20%">
+            <p className='text'><b>PROGRAMAÇÃO:</b><br/>
+             -java<br/>
+             -c#<br/>
+             -javascript<br/>
+             -css<br/>
+             -html<br/>
+            </p>
+          </TextBox>
+
+          <TextBox bg="#f1f1f1" width="20%">
+            <p className='text'><b>ARTE:</b><br/>
+             -nanquim<br/>
+             -aquarela<br/>
+             -grafite<br/>
+             -fotos<br/>
+             -vídeos<br/>
+            </p>
+          </TextBox>
+        </div>
+      </div>
     </div>
   );
 }
