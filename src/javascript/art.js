995933c81f5b190197b1757f9f7d5dfc2394c096
Kraschowetz <joao.kraschowetz@gmail.com>
2023-11-30 09:30:30 -0300

    upd

--- src/javascript/art.js
+++ src/javascript/art.js
@@ -4,7 +4,8 @@ import { IoFlowerSharp } from "react-icons/io5";
 import {Link} from 'react-router-dom';
 import { FaCode, FaPaintbrush  } from "react-icons/fa6";
 import { IoMdSchool } from "react-icons/io";
-//import * as exp from './exports.js';
+import { RiHome2Fill } from "react-icons/ri";
+import * as exp from './exports.js';
 
 
 function Art(){
@@ -35,33 +36,59 @@ function Art(){
   }); 
     return(
         <div className='container'>
-            <header>
-                <h1>
-                    SITE INVRIVEL
-                </h1>
+            <div id='header-fixer'>
+                <header className={vsbl}>
+                    <h1>
+                        ARTE
+                    </h1>
 
-            <div className='menu' onClick={changeMenu}>
-                <button>
-                    <IoFlowerSharp color='white' size={50} className='menu-logo'/>
-                </button>
+                    <div className='menu' onClick={changeMenu}>
+                        <button>
+                        <IoFlowerSharp color='white' size={50} className='menu-logo'/>
+                        </button>
+                    </div>
+                </header>
 
-            </div>
-            {menuState && (vsbl === 'header-visible')? (
-                <div className='dropdown'>
-                    <Link to='/arte'>
-                    <FaPaintbrush size={50} className='menu-item'/>
-                    </Link>
+                {menuState && (vsbl === 'header-visible')? (
+                    <div className='dropdown'>
+                        <Link to='/home'>
+                        <RiHome2Fill size={50} className='menu-item'/>
+                        </Link>
 
-                    <Link to='/programacao'>
-                    <FaCode size={50} className='menu-item'/>
-                    </Link>
+                        <Link to='/programacao'>
+                        <FaCode size={50} className='menu-item'/>
+                        </Link>
 
-                    <Link to='/escola'>
-                    <IoMdSchool size={50} className='menu-item'/>
-                    </Link>
-                </div>
-            ) : null}
-            </header>
+                        <Link to='/escola'>
+                        <IoMdSchool size={50} className='menu-item'/>
+                        </Link>
+                    </div>
+                                                                
+                ) : null}                                      
+            </div>   {/*FIM DO CABEÇALHO*/}    
+
+            <img src='img/madvillainy.png' alt='cover' className='img-frame'/>  
+
+            <div id='intro-div'>
+                <exp.Title color='white'>MINHA ARTE</exp.Title>
+
+                <exp.TextBox bg="#f1f1f1" width="50%">
+                    <exp.Text>
+                        <b>COMO Q É?</b><br/>
+                        Taller de concreto.<br/>
+                        Cachorro.<br/>
+                        Aingobórigô.<br/>
+                        Engobórirô.<br/>
+                        Bidofómarô.<br/>
+                        Bidosémiô.<br/>
+                        Bidosémiô.<br/>
+                        Gôretaliô.<br/>
+                        Donlíííí.<br/>
+                        Ismafóó (mafó)<br/>
+                    </exp.Text>
+                </exp.TextBox>
+            </div>                              
+
         </div>
     );
 
